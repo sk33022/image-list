4eff2c5936181052018fa4171bf1aa42412b0f19
sk33022 <53243995+sk33022@users.noreply.github.com>
2022-01-30 20:10:31 +0545

    Update ImageList.js

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const ImageList = (props) => {
    const images = props.images.map((imag) => {
-        return <img key={imag.id} src={ imag.webformatURL} alt='image' />
+        return <img key={imag.id} src={ imag.webformatURL}  />
     })
   return <div>
      {images}
